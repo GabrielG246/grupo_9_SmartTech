191b5a7b303e70acfcc7c214cfabe0ccb78d2699
GabrielG246 <garcia-gabriel-246@hotmail.com>
2021-12-10 15:00:08 -0300

    Nuevo area de trabajo

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express= require("express");
 const app= express();
 const path= require("path");
+
+app.listen(3000, ()=> {
+    console.log("Server is running in port: 3000")
+});
+
+app.use(express.static("public"));
+
+app.get("/" ,(req, res) => {
+    res.sendFile(path.resolve("./views/index.html"))
+})

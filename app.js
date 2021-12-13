8483869f1839b659b57991c98f95b018739f530a
GabrielG246 <garcia-gabriel-246@hotmail.com>
2021-12-13 00:07:29 -0300

    Nuevas modificaciones

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const express= require("express");
-const app= express();
-const path= require("path");
+const express = require("express");
+const { get } = require("express/lib/response");
+const app = express();
+const path = require("path");
 
-app.listen(3000, ()=> {
-    console.log("Server is running in port: 3000")
+app.listen(3000, ()=>{
+    console.log("Servidor corriendo en puerto 3000")
 });
 
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-app.get("/" ,(req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.resolve("./views/index.html"))
+})
+
+app.get("/carrito.html", (req, res) => {
+    res.sendFile(path.resolve("./views/carrito.html"))
+})
+
+app.get("/producto.html", (req, res) => {
+    res.sendFile(path.resolve("./views/producto.html"))
 })
